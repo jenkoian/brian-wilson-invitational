a0cf96b959fb377b71aa9bb0f17657904820f2a9
Ian Jenkins <ian@jenkins-web.co.uk>
2025-12-22 16:14:45 +0000

    Round 13

--- builddb.py
+++ builddb.py
@@ -2,12 +2,31 @@ import zipfile
 import duckdb
 import os
 import shutil
+import time
 from dotenv import load_dotenv
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 import pylast
+
+
+def retry_api_call(max_retries=3, delay=2):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            retries = 0
+            while retries < max_retries:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    retries += 1
+                    if retries == max_retries:
+                        print(f"Failed after {max_retries} attempts: {e}")
+                        raise
+                    print(f"Error encountered: {e}. Retrying in {delay}s... ({retries}/{max_retries})")
+                    time.sleep(delay)
+        return wrapper
+    return decorator
 
 
 def add_genre_spotify(table_name: str):
@@ -18,19 +37,25 @@ def add_genre_spotify(table_name: str):
 
     songs = con.execute(query).fetchall()
 
-    for uri in songs:
-        song = sp.track(uri[0])
-
-        print(f"Getting genre for {song['name']} by {song['artists'][0]['name']} ({song['artists'][0]['id']})")
-
+    @retry_api_call(max_retries=5, delay=3)
+    def fetch_spotify_data(uri):
+        song = sp.track(uri)
         artist_ids = [artist['id'] for artist in song['artists']]
         artists = sp.artists(artist_ids)
+        return song, artists
 
-        genres = [artist['genres'] for artist in artists['artists']]
+    for uri in songs:
+        try:
+            song, artists = fetch_spotify_data(uri[0])
+            print(f"Getting genre for {song['name']} by {song['artists'][0]['name']}")
 
-        genre = genres[0]
-        query = f"UPDATE {table_name} SET spotify_genres = {genre} WHERE \"Spotify URI\" = '{uri[0]}'"
-        con.execute(query)
+            genres = [artist['genres'] for artist in artists['artists']]
+
+            genre = genres[0]
+            query = f"UPDATE {table_name} SET spotify_genres = {genre} WHERE \"Spotify URI\" = '{uri[0]}'"
+            con.execute(query)
+        except Exception as e:
+            print(f"Skipping {uri[0]} due to persistent error.")
 
 
 def add_genre_lastfm(table_name: str):
@@ -48,16 +73,23 @@ def add_genre_lastfm(table_name: str):
 
     songs = con.execute(query).fetchall()
 
+    @retry_api_call(max_retries=5, delay=2)
+    def fetch_lastfm_data(artist, title):
+        track = lf.get_track(artist, title)
+        return track, track.get_top_tags()
+
     for song in songs:
-        track = lf.get_track(song[0], song[1])
-        tags = track.get_top_tags()
+        try:
+            track, tags = fetch_lastfm_data(song[0], song[1])
 
-        print(f"Getting genre for {track}")
+            print(f"Getting genre for {track}")
 
-        genres = [tag.item.get_name().lower() for tag in tags[:2]]
+            genres = [tag.item.get_name().lower() for tag in tags[:2]]
 
-        query = f"UPDATE {table_name} SET lastfm_genres = {genres} WHERE \"Spotify URI\" = '{song[2]}'"
-        con.execute(query)
+            query = f"UPDATE {table_name} SET lastfm_genres = {genres} WHERE \"Spotify URI\" = '{song[2]}'"
+            con.execute(query)
+        except Exception as e:
+            print(f"Skipping {song[0]} - {song[1]} due to Last.fm error.")
 
 
 print('Unzipping export.zip...')
